19123a7cd5f60c065420ee8034aed595ac6f91a1
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-01-20 09:54:33 -0600

    inital buttons built

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Button from './components/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      current: '0',
+      previous: []
+    };
+  }
+
+  reset = () => {
+    this.setState({ result: '0' });
+  };
+
+  addToCurrent = symbol => {
+    this.setState({ current: this.state.current + symbol });
+  };
+
+  render() {
+    const buttons = [
+      { symbol: 'C', cols: 3, action: this.reset },
+      { symbol: '/', cols: 1, actions: this.addToCurrent },
+      { symbol: '7', cols: 1, actions: this.addToCurrent },
+      { symbol: '8', cols: 1, actions: this.addToCurrent },
+      { symbol: '9', cols: 1, actions: this.addToCurrent },
+      { symbol: 'X', cols: 1, actions: this.addToCurrent },
+      { symbol: '4', cols: 1, actions: this.addToCurrent },
+      { symbol: '5', cols: 1, actions: this.addToCurrent },
+      { symbol: '6', cols: 1, actions: this.addToCurrent },
+      { symbol: '-', cols: 1, actions: this.addToCurrent },
+      { symbol: '1', cols: 1, actions: this.addToCurrent },
+      { symbol: '2', cols: 1, actions: this.addToCurrent },
+      { symbol: '3', cols: 1, actions: this.addToCurrent },
+      { symbol: '+', cols: 1, actions: this.addToCurrent },
+      { symbol: '/', cols: 1, actions: this.addToCurrent },
+      { symbol: '0', cols: 2, actions: this.addToCurrent },
+      { symbol: '.', cols: 1, actions: this.addToCurrent },
+      { symbol: '=', cols: 1, actions: this.addToCurrent }
+    ];
+
+    return (
+      <div className='App'>
+        <input className='result' type='text' value={this.state.current} />
+
+        {buttons.map((btn, i) => {
+          return (
+            <Button
+              symbol={btn.symbol}
+              cols={btn.cols}
+              action={symbol => btn.action}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
